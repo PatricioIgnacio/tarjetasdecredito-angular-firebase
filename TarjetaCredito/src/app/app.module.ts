94e4716c7f948b75ef5a34d6c2b2e0e594d6ed85
PatricioIgnacio <patricioignacio.santibanezgonz@gmail.com>
2023-08-24 21:31:07 -0400

    TypeScript component Crear Tarjeta

--- TarjetaCredito/src/app/app.module.ts
+++ TarjetaCredito/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,9 @@ import { CreateCardComponent } from './components/create-card/create-card.compon
 import { ListCardComponent } from './components/list-card/list-card.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CreateCardComponent,
-    ListCardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CreateCardComponent, ListCardComponent],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- TarjetaCredito/src/app/components/create-card/create-card.component.ts
+++ TarjetaCredito/src/app/components/create-card/create-card.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-card',
   templateUrl: './create-card.component.html',
-  styleUrls: ['./create-card.component.css']
+  styleUrls: ['./create-card.component.css'],
 })
 export class CreateCardComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta: ['', Validators.required],
+      fechaExpiracion: ['', Validators.required],
+      cvv: ['', Validators.required],
+    });
+  }
+
+  crearTarjeta() {
+    console.log(this.form);
+  }
 }
